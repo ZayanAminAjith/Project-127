d24d3007c02a014eba33e4e393c32c927f0c3f08
ZayanAminAjith <96104239+ZayanAminAjith@users.noreply.github.com>
2023-08-09 13:45:02 +0400

    Update updated_scraper.py

--- updated_scraper.py
+++ updated_scraper.py
@@ -23,22 +23,34 @@ def scrape():
 
         for row in table_rows:
             table_cols = row.find_all("td")
-            # print(table_cols)
+            print(table_cols)
             temp_list = []
             for col_data in table_cols:
-                # print(col_data.text)
+                print(col_data.text)
                 data = col_data.text.strip()
+                print(data)
                 temp_list.append(data)
 
         scraped_data.append(temp_list)
 # Calling Method
 scrape()
 
+stars_data = []
+for i in range(0,len(scraped_data)):
+    Star_names = scraped_data[i][1]
+    Distance = scraped_data[i][3]
+    Mass = scraped_data[i][5]
+    Radius = scraped_data[i][6]
+    Lum = scraped_data[i][7]
+
+    required_data = [Star_names, Distance, Mass, Radius, Lum]
+    stars_data.append(required_data)
+
 # Define Header
-headers = ["Star_name", "Distance", "Radius", "Mass", "Luminosity"]
+headers = ["Star_names","Distance","Mass","Radius","Luminosity"]
 
 # Define pandas DataFrame 
-stars_df_1 = pd.DataFrame(scraped_data, columns=headers)
+stars_df_1 = pd.DataFrame(stars_data, columns=headers)
 
 # Convert to CSV
 stars_df_1.to_csv('updated_scraped_data.csv',index=True, index_label="id")
